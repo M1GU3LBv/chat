4b6d0575bfc742cbe99e6776beccf2b1de9f2381
M1GU3LBv <anllacarpro@gmail.com>
2023-04-16 21:01:05 -0500

    sssd

--- .history/chatapp_20210218162927.sql
+++ .history/chatapp_20210218162927.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2023 at 04:00 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Feb 18, 2021 at 11:44 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,23 +31,8 @@ CREATE TABLE `messages` (
   `msg_id` int(11) NOT NULL,
   `incoming_msg_id` int(255) NOT NULL,
   `outgoing_msg_id` int(255) NOT NULL,
-  `msg` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `imgs` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`, `imgs`) VALUES
-(1, 1559106529, 646888232, 'Hola', NULL),
-(2, 646888232, 1559106529, '😂a', '1681662317c667179d3de776412b61c60c76aeabaa.png'),
-(3, 646888232, 1559106529, 'a', NULL),
-(4, 646888232, 1559106529, 'aaa', NULL),
-(5, 646888232, 1559106529, 'Hola', ''),
-(6, 646888232, 1559106529, 'a', ''),
-(8, 646888232, 1559106529, 'asd', '1681695819379512_chat_icon.png'),
-(9, 646888232, 1559106529, 'Hola😃', '1681696620379512_chat_icon.png');
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -64,15 +49,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `img` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`) VALUES
-(1, 1559106529, 'Miguel', 'Alarcon', 'anllacarpro@gmail.com', '202cb962ac59075b964b07152d234b70', '1681662317c667179d3de776412b61c60c76aeabaa.png', 'Active now'),
-(2, 646888232, 'Test', 'Nashe', 'lanzapapa124@gmail.com', '202cb962ac59075b964b07152d234b70', '1681662350Fr2ZpVYWYAILV6W.jpeg', 'Active now');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -98,13 +75,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
